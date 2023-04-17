7f77fea96a7257a7825e0e76d99268b82c79cb3a
allen <allenw@sfu.ca>
2023-04-17 02:20:43 -0700

    Initial Commit

---
+++ VertTracker/VertTrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  VertTrackerApp.swift
+//  VertTracker
+//
+//  Created by Allen Wong on 2023-04-17.
+//
+
+import SwiftUI
+
+@main
+struct VertTrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
